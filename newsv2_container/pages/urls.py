0da4158c29882ad90259568b9035ec81c4a431d7
iozberk <iozberks@gmail.com>
2022-04-13 23:29:19 +0300

    index.html load statics done

--- newsv2_container/pages/urls.py
+++ newsv2_container/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from pages.views import IndexView
+from pages.views import IndexView, NewsView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
+    path('news/', NewsView.as_view(), name="news"),
 ]
--- newsv2_container/pages/views.py
+++ newsv2_container/pages/views.py
@@ -5,3 +5,8 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+
+class NewsView(TemplateView):
+    template_name = 'news.html'
